59c0c408edfccb04c993513e605632eecd7cfe46
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-05-22 11:42:24 -0600

    data.length solution

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -8,19 +8,24 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useQuery } from '@tanstack/react-query';
 import { makeRequest } from "/services/axios";
 import moment from "moment";
+import { AuthContext } from "src/context/authContext";
 
 const Post = ({post}) => {
   const [commentOpen, setCommentOpen] = useState(false)
 
+  const { currentUser } = useContext(AuthContext);
+
   //Likes
+  //Issue: TypeError for data when using data.length
+  //Solution: Add a check first to confirm data then process data.length
+  //(data && data.length)
   const { isLoading, error, data } = useQuery(["likes", post.id], () => 
     makeRequest.get("/likes?postId=" + post.id).then((res) => {
-      length = (res.data).length
-      return res.data, length;
+      return res.data;
     })
   );
 
@@ -49,12 +54,12 @@ const Post = ({post}) => {
         </div>
         <div className="info">
           <div className="item">
-            {/* {liked ? (
+            {data && data.includes(currentUser.id) ? (
               <FavoriteOutlinedIcon style={{color:'red'}} />
               ) : (
               <FavoriteBorderOutlinedIcon /> 
-            )} */}
-            { data } likes
+            )}
+            { data && data.length } likes
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
--- server/services/mainAPI/controllers/like.js
+++ server/services/mainAPI/controllers/like.js
@@ -6,8 +6,9 @@ export const getLikes = (req, res) => {
   const q = "SELECT userId FROM likes WHERE postId = ?";
 
   //Query Post(via postid) for like status
+  //Map through data and only send userId to frontend
   db.query(q, [req.query.postId], (err, data) => {
     if (err) return res.status(500).json(err);
-    return res.status(200).json(data);
+    return res.status(200).json(data.map(like=>like.userId));
   });
 };
