303dd58bec9947d2ef05d726d273bc3d03141416
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-05-28 19:00:11 -0600

    blog start

---
+++ client/src/pages/blog/Article.jsx
@@ -0,0 +1,9 @@
+import "/src/assets/css/pages/blog/article.scss";
+
+const Article = () => {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article;
---
+++ client/src/pages/blog/Articles.jsx
@@ -0,0 +1,9 @@
+import "/src/assets/css/pages/blog/articles.scss";
+
+const Articles = () => {
+  return (
+    <div>articles</div>
+  )
+}
+
+export default Articles;
