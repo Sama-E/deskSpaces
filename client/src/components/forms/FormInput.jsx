b4b193f120cb6491f031a306745e2c327b7a8c40
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-06-27 12:30:30 -0600

    validations

---
+++ client/src/components/forms/FormInput.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "/src/assets/css/pages/auth/forminput.scss";
+
+const FormInput = (props) => {
+  //Destructure
+  const {onChange, errorMessage, id, ...inputProps } = props;
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="formInput">
+    <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  )
+}
+
+export default FormInput;
