25f17d1c6a1b0073888b21a2eb2965c863c6f70a
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-06-30 20:44:37 -0600

    blog update

--- client/src/pages/blog/BlogPost.jsx
+++ client/src/pages/blog/BlogPost.jsx
@@ -4,11 +4,18 @@ const BlogPost = () => {
   return (
     <div className="blogPost">
       <div className="content">
-        C
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+          <div className="user">
+            <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+            <div className="info">
+              <p>John</p>
+              <p>Posted 2 days ago</p>
+            </div>
+          </div>
+        </div>
         <div className="menu">
           M
         </div>
-      </div>
     </div>
   )
 }
