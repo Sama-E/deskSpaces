8d89d54b700da1a92f24639f0c862f347a05db02
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-07-03 09:40:08 -0600

    blog update

--- client/src/pages/blog/BlogPostNew.jsx
+++ client/src/pages/blog/BlogPostNew.jsx
@@ -3,6 +3,39 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const Categories = [
+  {
+    id: 1,
+    name: "technology",
+    title:"Technology",
+  },
+  {
+    id: 2,
+    name: "algorithms",
+    title:"Algorithms",
+  },
+  {
+    id: 3,
+    name: "real_estate",
+    title:"Real Estate",
+  },
+  {
+    id: 4,
+    name: "food",
+    title:"Food",
+  },
+  {
+    id: 5,
+    name: "economics",
+    title:"Economics",
+  },
+  {
+    id: 6,
+    name: "blockchain",
+    title:"BlockChain",
+  },
+]
+
 const BlogPostNew = () => {
   const [value, setValue] = useState('');
 
@@ -14,12 +47,34 @@ console.log(value)
       <div className="content">
         <input type="text" placeholder="Title" />
         <div className="editorContainer">
-          <ReactQuill theme="snow" value={value} onChange={setValue} />
+          <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
         </div>
       </div>
       <div className="menu">
-        <div className="item">i1</div>
-        <div className="item">i2</div>
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display:"none" }} type="file" id="file" name=""/>
+          <label className="file" htmlFor="file">Upload Image</label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          {Categories.map(category => (
+            <div className="cat">
+              <input key={category.id} type="radio" name="cat" value={category.name} id={category.name} />
+              <label htmlFor={category.name}>{category.title}</label>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
